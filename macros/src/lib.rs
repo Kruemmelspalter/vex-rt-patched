332fd4a815f25433831fa49bd68a3185b3d0afd1
Nick Mertin <nickmertin@gmail.com>
2022-12-16 18:34:32 -0500

    Cleanup

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -3,6 +3,6 @@ use proc_macro::TokenStream;
 mod state_machine;
 
 #[proc_macro]
-pub fn make_state_machine(item: TokenStream) -> TokenStream {
-    state_machine::make_state_machine(item)
+pub fn make_state_machine(input: TokenStream) -> TokenStream {
+    state_machine::make_state_machine(input)
 }
--- macros/src/state_machine/mod.rs
+++ macros/src/state_machine/mod.rs
@@ -6,12 +6,9 @@ use crate::state_machine::input::Input;
 mod input;
 
 pub fn make_state_machine(input: TokenStream) -> TokenStream {
-    // let arguments = parse_macro_input!(args as Arguments);
-    // let body = parse_macro_input!(item as ItemMod);
     let input = parse_macro_input!(input as Input);
 
     println!("{:#?}", input);
 
-    // quote!(#body).into()
     todo!()
 }
