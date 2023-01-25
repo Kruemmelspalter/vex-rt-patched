afa3619871efb8f61f0e557b4a72114f98e0c4eb
Nick Mertin <nickmertin@gmail.com>
2023-01-25 16:15:49 -0500

    Continue working on documentation

--- src/macros/state_machine.rs
+++ src/macros/state_machine.rs
@@ -6,7 +6,7 @@
 /// state definitions. The state machine definition has the syntax:
 /// ```text
 /// <visibility> <TypeName>(<generics>)? (<parethesized_parameter_list>)? ({
-///     (<field_definition> = <initializer>,)*
+///     (<field_definition> = <initializer>),*
 /// })? = <initial_state>;
 /// ```
 ///
@@ -21,6 +21,23 @@
 /// `<initial state>` is the name of a state (see below), followed by a
 /// parenthesized argument list (for which the parameters are in scope) if the
 /// state has any parameters.
+///
+/// Each state definition has the syntax:
+/// ```text
+/// <name>(<ctx> (, (<param>),*)?) ([(<field_pat>),*])? (-> <return_type>)? {
+///     <body>
+/// }
+/// ```
+///
+/// Here, `<name>` is the name of the state (which should be in `snake_case`);
+/// `<ctx>` is the name for a parameter which will be passed the
+/// [`Context`](crate::rtos::Context) for the state execution; each `<param>` is
+/// a parameter definition, as in a function signature; each `<field_pat>` is a
+/// field pattern for destructuring a `struct` containing the fields defined in
+/// the state machine definition; `<return_type>` is the return type, which is
+/// `()` if omitted, as with ordinary functions; and `<body>` is the function
+/// body which implements the state behaviour, for which the state parameters
+/// and field patterns are in scope.
 macro_rules! state_machine {
     ($($args:tt)*) => {
         $crate::macros::make_state_machine!($crate; $($args)*);
