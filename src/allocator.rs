8e32a88086598d26e2bdfdb6ab27e9f605139d1a
Nick Mertin <nickmertin@gmail.com>
2023-02-15 20:34:58 -0500

    Patchfix to make rust allocator thread-safe

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,5 +1,28 @@
-use core::alloc::Layout;
-use newlib_alloc::Alloc;
+use core::alloc::{GlobalAlloc, Layout};
+
+use libc::{free, memalign};
+
+struct Alloc;
+
+unsafe impl GlobalAlloc for Alloc {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        __malloc_lock();
+        let mem = memalign(layout.align(), layout.size()) as *mut _;
+        __malloc_unlock();
+        mem
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        __malloc_lock();
+        free(ptr as *mut _);
+        __malloc_unlock();
+    }
+}
+
+extern "C" {
+    fn __malloc_lock();
+    fn __malloc_unlock();
+}
 
 #[global_allocator]
 static ALLOCATOR: Alloc = Alloc;
